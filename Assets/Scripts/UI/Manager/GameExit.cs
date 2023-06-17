e6e2a8088be03c4fc63253a25c5dc507ba5a3983
SilverBe11 <eunjongh97@gmail.com>
2023-06-18 00:08:24 +0900

    게임 클리어 후 코인 획득

--- Assets/Scripts/UI/Manager/GameExit.cs
+++ Assets/Scripts/UI/Manager/GameExit.cs
@@ -12,7 +12,16 @@ public class GameExit : MonoBehaviour
     public int checkedHP;
     private int checkedWave;
     public Action OnActivateResultWindow;
+    GameManager gameManager;
+    SaveManager saveManager;
+    MissionManager missionManager;
 
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        saveManager = GameObject.Find("SaveManager").GetComponent<SaveManager>();
+        missionManager = GameObject.Find("MissionManager").GetComponent<MissionManager>();
+    }
     private void OnEnable()
     {
         playerHQ.OnHpChange += UpdateCheckedHP;
@@ -43,7 +52,9 @@ public class GameExit : MonoBehaviour
     private void ActivateResultWindow()
     {
         Debug.Log("Å¬¸®¾î!");
+        gameManager.coin += 4;
         ResultWindow.transform.GetChild(1).gameObject.SetActive(true);
         OnActivateResultWindow?.Invoke();
+        saveManager.Save();
     }
 }
